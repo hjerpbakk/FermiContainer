6eb590f941199f7e77e61073e8beffd17601d634
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2020-12-02 20:44:38 +0100

    Updated to .Net Standard 2.0

---
+++ FermiContainer.Standard/FermiContainer.cs
@@ -0,0 +1,157 @@
+/******************************************************************************
+The MIT License (MIT)
+
+Copyright (c) 2014 Runar Ovesen Hjerpbakk
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+******************************************************************************
+    FermiContainer version 1.1.1
+    http://www.hjerpbakk.com/fermicontainer
+    https://twitter.com/hjerpbakk
+******************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading;
+
+namespace Hjerpbakk.FermiContainer
+{
+    public class FermiContainer : IFermiContainer
+    {
+        private static readonly Lazy<IFermiContainer> defaultInstance;
+
+        protected readonly Dictionary<Type, Service> Services;
+
+        static FermiContainer()
+        {
+            defaultInstance = new Lazy<IFermiContainer>(() => new FermiContainer());
+        }
+
+        public FermiContainer()
+        {
+            Services = new Dictionary<Type, Service>();
+        }
+
+        public static IFermiContainer DefaultInstance { get { return defaultInstance.Value; } }
+
+        public void Register<TInterface, TClass>() where TClass : class, TInterface
+        {
+            var type = typeof(TClass);
+            var ctor = type.GetConstructors()[0];
+            var neededParameters = ctor.GetParameters();
+            var n = neededParameters.Length;
+            var arguments = new Expression[n];
+            for (int i = 0; i < n; i++)
+            {
+                var argumentType = neededParameters[i].ParameterType;
+                Expression<Func<object>> getService = () => Services[argumentType].Factory();
+                arguments[i] = Expression.Convert(getService.Body, argumentType);
+            }
+
+            var newExpression = Expression.New(ctor, arguments);
+            var newAsLambda = Expression.Lambda<Func<object>>(newExpression);
+            Services.Add(typeof(TInterface), new Service(newAsLambda.Compile()));
+        }
+
+        public void Register<TClass>() where TClass : class
+        {
+            Register<TClass, TClass>();
+        }
+
+        public void Register<TInterface>(Func<object> factory)
+        {
+            Services.Add(typeof(TInterface), new Service(factory));
+        }
+
+        public TInterface Resolve<TInterface>() where TInterface : class
+        {
+            return (TInterface)Services[typeof(TInterface)].Factory();
+        }
+
+        public TInterface Singleton<TInterface>() where TInterface : class
+        {
+            var service = Services[typeof(TInterface)];
+            if (Interlocked.Exchange(ref service.SingletonInitialized, 1) == 0)
+            {
+                var value = (TInterface)service.Factory();
+                service.Factory = () => value;
+                return value;
+            }
+
+            return (TInterface)service.Factory();
+        }
+
+        protected class Service
+        {
+            public int SingletonInitialized;
+            public Func<object> Factory;
+
+            public Service(Func<object> factory)
+            {
+                Factory = factory;
+            }
+        }
+    }
+
+    /// <summary>
+    ///  Defines a set of methods used to register services into the service container.
+    /// </summary>
+    public interface IFermiContainer
+    {
+        /// <summary>
+        /// Registers an interface with a given implementing class in the container.
+        /// The first constructor of the implementing class is used, and constructor
+        /// arguments are automatically resolved using the container.
+        /// </summary>
+        /// <typeparam name="TInterface">The interface which the class satisfies.</typeparam>
+        /// <typeparam name="TClass">The implementing class of the interface.</typeparam>
+        void Register<TInterface, TClass>() where TClass : class, TInterface;
+
+        /// <summary>
+        /// Registers a class without an interface in the container.
+        /// </summary>
+        /// <typeparam name="TClass">The class to be registered.</typeparam>
+        void Register<TClass>() where TClass : class;
+
+        /// <summary>
+        /// Registers an implementing class in the container using a factory method.
+        /// Use this if your implementation has dependencies not present
+        /// in the container.
+        /// </summary>
+        /// <param name="factory">The factory method to use.</param>
+        /// <typeparam name="TInterface">The interface which the class satisfies.</typeparam>
+        /// <typeparam name="TClass">The implementing class of the interface.</typeparam>
+        // Analysis disable once UnusedTypeParameter
+        void Register<TInterface>(Func<object> factory);
+
+        /// <summary>
+        /// Returns the registered implementing class of the given interface.
+        /// </summary>
+        /// <typeparam name="TInterface">The interface from which to get the implementation.</typeparam>
+        TInterface Resolve<TInterface>() where TInterface : class;
+
+        /// <summary>
+        /// Returns the registered implementing class of the given interface as a singleton.
+        /// This method will always return the same instance for a given interface.
+        /// </summary>
+        /// <typeparam name="TInterface">The interface from which to get the implementation.</typeparam>
+        TInterface Singleton<TInterface>() where TInterface : class;
+    }
+}
+
---
+++ FermiContainer.Tests/Tests.cs
@@ -0,0 +1,171 @@
+using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Hjerpbakk.FermiContainer;
+using Xunit;
+
+namespace FermiContainer.Tests
+{
+    public class Tests
+    {
+        readonly IFermiContainer fermiContainer;
+
+        public Tests()
+        {
+            fermiContainer = new Hjerpbakk.FermiContainer.FermiContainer();
+        }
+
+        [Fact]
+        public void Resolve_ClassRegistered_ReturnsNewObject()
+        {
+            fermiContainer.Register<ICalculator, Calculator>();
+
+            var calculator = fermiContainer.Resolve<ICalculator>();
+
+            calculator.Should().BeAssignableTo<Calculator>();
+        }
+
+        [Fact]
+        public void Resolve_ClassNotRegistered_ThrowsException()
+        {
+            Action retriveNonRegisteredInstance = () => fermiContainer.Resolve<ICalculator>();
+
+            retriveNonRegisteredInstance.Should().Throw<KeyNotFoundException>();
+        }
+
+        [Fact]
+        public void ResolveTwoTimes_ClassRegistered_TwoObjectsCreated()
+        {
+            fermiContainer.Register<ICalculator, Calculator>();
+            var calculator = fermiContainer.Resolve<ICalculator>();
+
+            var calculator2 = fermiContainer.Resolve<ICalculator>();
+
+            calculator.Should().NotBeSameAs(calculator2);
+        }
+
+        [Fact]
+        public void Singleton_ClassNotRegistered_ThrowsException()
+        {
+            Action nonRegisteredClassAsSingleton = () => fermiContainer.Singleton<ICalculator>();
+
+            nonRegisteredClassAsSingleton.Should().Throw<KeyNotFoundException>();
+        }
+
+        [Fact]
+        public void SingletonTwoTimes_ClassRegistered_OneObjectCreated()
+        {
+            fermiContainer.Register<ICalculator, Calculator>();
+            var calculator = fermiContainer.Singleton<ICalculator>();
+
+            var calculator2 = fermiContainer.Singleton<ICalculator>();
+
+            calculator.Should().BeSameAs(calculator2);
+        }
+
+        [Fact]
+        public void Resolve_ClassRegisteredWithCustomCtor_ReturnsNewObject()
+        {
+            fermiContainer.Register<ICalculator>(() => new Calculator());
+
+            var calculator = fermiContainer.Resolve<ICalculator>();
+
+            calculator.Should().BeAssignableTo<Calculator>();
+        }
+
+        [Fact]
+        public void Resolve_RegisteredPremadeObject_ReturnsTheObject()
+        {
+            var calculator = new Calculator();
+            fermiContainer.Register<ICalculator>(() => calculator);
+
+            var calculator2 = fermiContainer.Resolve<ICalculator>();
+
+            calculator.Should().BeSameAs(calculator2);
+        }
+
+        [Fact]
+        public void Register_ComplexClassWithFactory_CanBeResolved()
+        {
+            fermiContainer.Register<ICalculator, Calculator>();
+            fermiContainer.Register<IComplex>(() => new ComplexClass(fermiContainer.Resolve<ICalculator>()));
+
+            var complexInstance = fermiContainer.Resolve<IComplex>();
+
+            complexInstance.Should().BeAssignableTo<IComplex>();
+        }
+
+        [Fact]
+        public void Register_ComplexClass_CanBeResolved()
+        {
+            fermiContainer.Register<ICalculator, Calculator>();
+            fermiContainer.Register<IComplex, ComplexClass>();
+
+            var complexInstance = fermiContainer.Resolve<IComplex>();
+
+            complexInstance.Should().BeAssignableTo<IComplex>();
+        }
+
+        [Fact]
+        public void Register_EvenMoreComplexClass_CanBeResolved()
+        {
+            fermiContainer.Register<IEvenMoreComplex, EvenMoreComplex>();
+            fermiContainer.Register<ICalculator, Calculator>();
+            fermiContainer.Register<IComplex, ComplexClass>();
+            fermiContainer.Register<ClassWithoutInterface>();
+
+            var complexInstance = fermiContainer.Resolve<IEvenMoreComplex>();
+
+            complexInstance.Should().BeAssignableTo<IEvenMoreComplex>();
+        }
+
+        [Fact]
+        public void Register_InterfaceAlreadyRegistered_ThrowsException()
+        {
+            fermiContainer.Register<ICalculator, Calculator>();
+
+            Action secondRegistration = () => fermiContainer.Register<ICalculator, Calculator>();
+
+            secondRegistration.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void Register_OnlyImplementation_CanBeResolved()
+        {
+            fermiContainer.Register<ClassWithoutInterface>();
+
+            var classWithoutInterface = fermiContainer.Resolve<ClassWithoutInterface>();
+
+            classWithoutInterface.Should().BeAssignableTo<ClassWithoutInterface>();
+        }
+
+        [Fact]
+        public void DefaultInstance_Used_IsOnlyOneInstance()
+        {
+            var instance = Hjerpbakk.FermiContainer.FermiContainer.DefaultInstance;
+
+            instance.Should().BeAssignableTo<IFermiContainer>();
+            instance.Should().BeSameAs(Hjerpbakk.FermiContainer.FermiContainer.DefaultInstance);
+        }
+    }
+
+    public interface ICalculator { }
+
+    public class Calculator : ICalculator { }
+
+    public interface IComplex { }
+
+    public class ComplexClass : IComplex
+    {
+        public ComplexClass(ICalculator calculator) { }
+    }
+
+    public interface IEvenMoreComplex { }
+
+    public class EvenMoreComplex : IEvenMoreComplex
+    {
+        public EvenMoreComplex(IComplex complex, ICalculator calculator, ClassWithoutInterface classWithoutInterface) { }
+    }
+
+    public class ClassWithoutInterface { }
+}
